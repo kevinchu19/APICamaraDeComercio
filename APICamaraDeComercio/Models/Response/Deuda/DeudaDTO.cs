aac6407123761ba2b8588cdb15b812996169c950
Kevin Tchubinsky <kevinchu19@gmail.com>
2024-01-29 02:06:28 -0300

    Se agrega calculo de vencimientos en endpoint deuda para ipcac

--- APICamaraDeComercio/Models/Response/Deuda/DeudaDTO.cs
+++ APICamaraDeComercio/Models/Response/Deuda/DeudaDTO.cs
@@ -8,6 +8,13 @@
         public DateTime fechaVencimiento { get; set; }
         public decimal importe { get; set; }
         public string generador { get; set; }
+        public decimal? importePrimerVenc { get; set; }
+        public decimal? importeSegundoVenc { get; set; }
+        public decimal? importeTercerVenc { get; set; }
+        public DateTime? fechaSegundoVenc { get; set; }
+        public DateTime? fechaTercerVenc { get; set; }
+        public string? leyendaImporte { get; set; }
+
 
 
     }
