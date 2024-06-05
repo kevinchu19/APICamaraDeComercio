36e9b78e900d3d56e1cde60c1596a0c5ade9de81
Kevin Tchubinsky <kevinchu19@gmail.com>
2024-06-05 11:03:12 -0300

    Se agrega llamado a insertar SAR_FCRMVD en endpoint facturacion para SIGEDU

--- APICamaraDeComercio/Repositories/FacturacionRepository.cs
+++ APICamaraDeComercio/Repositories/FacturacionRepository.cs
@@ -1,11 +1,52 @@
-﻿namespace APICamaraDeComercio.Repositories
+﻿using APICamaraDeComercio.Services.Entities;
+using Microsoft.Data.SqlClient;
+
+namespace APICamaraDeComercio.Repositories
 {
     public class FacturacionRepository: RepositoryBase
     {
         public FacturacionRepository(IConfiguration configuration) : base(configuration)
         {
         }
+        public override async Task<string> ExecuteSqlInsertToTablaSAR(List<FieldMap> fieldMapList, object resource, object valorIdentificador, string jobName)
+        {
+            string errorMessage = "";
 
+            errorMessage = await base.ExecuteSqlInsertToTablaSAR(fieldMapList, resource, valorIdentificador, jobName);
+
+            if (errorMessage == "")
+            {
+                try
+                {
+
+                    Alm_InsSAR_FCRMVDFromSAR_FCRMVH((string)valorIdentificador);
+
+                }
+                catch (Exception ex)
+                {
+
+                    return ex.Message + ex.StackTrace;
+                }
+            }
+
+            return errorMessage;
+        }
+        private async Task Alm_InsSAR_FCRMVDFromSAR_FCRMVH(string identificador)
+        {
+            using (SqlConnection sql = new SqlConnection(Configuration.GetConnectionString("DefaultConnectionString")))
+            {
+                using (SqlCommand cmd = new SqlCommand("Alm_InsSAR_FCRMVDFromSAR_FCRMVH", sql))
+                {
+
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@Identificador", identificador));
+
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+
+                }
+            }
+        }
 
     }
 }
